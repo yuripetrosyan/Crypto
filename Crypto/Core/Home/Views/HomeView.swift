b3279639d6c72d5c61bb0874434478c18f7d909e
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-12-01 18:56:19 +0100

    Add columnTitles

--- Crypto/Core/Home/Views/HomeView.swift
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     
@@ -21,12 +22,18 @@ struct HomeView: View {
             VStack{
                 
                 homeHeader
-               
-                List{
-                    CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingsColumn: false)
-                }.listStyle(PlainListStyle())
                 
+                columnTitles
                 
+                if !showPortfolio {
+                    allCoinsList
+                    .transition(.move(edge: .leading))
+                    
+                }
+                if showPortfolio{
+                    portfolioCoinsList
+                        .transition(.move(edge: .trailing))
+                }
                 Spacer(minLength: 0)
                 
                 
@@ -37,10 +44,14 @@ struct HomeView: View {
     }
 }
 
-#Preview {
-    NavigationView{
-        HomeView()
-            .navigationBarHidden(true)
+
+struct HomeView_Previews: PreviewProvider{
+    static var previews: some View{
+        NavigationView{
+            HomeView()
+                .navigationBarHidden(true)
+        }
+        .environmentObject(dev.homeVM)
     }
 }
 
@@ -75,5 +86,38 @@ extension HomeView {
                 }
 
         }.padding(.horizontal)
+    }
+    private var allCoinsList: some View {
+        List{
+            ForEach(vm.allCoins){ coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var portfolioCoinsList: some View {
+        List{
+            ForEach(vm.portfolioCoins){ coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+            }
+        }
+        .listStyle(PlainListStyle())
+    }
+    
+    private var columnTitles: some View {
+        HStack{
+            Text("Coin")
+            Spacer()
+            if showPortfolio{
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }.font(.caption)
+            .foregroundStyle(Color.theme.secondaryText)
+            .padding(.horizontal)
     }
 }
--- Crypto/CryptoApp.swift
+++ Crypto/CryptoApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct CryptoApp: App {
+    
+    @StateObject private var vm = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             
@@ -16,6 +19,7 @@ struct CryptoApp: App {
                 HomeView()
                     .navigationBarHidden(true)
             }
+            .environmentObject(vm)
         }
     }
 }
